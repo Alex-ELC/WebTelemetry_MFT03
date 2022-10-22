3a197228753f78ebeb5325530d227715f13d3b14
alex13217 <alexgilbert215@gmail.com>
2022-10-22 14:31:45 +0200

    Provisional models

--- liveview/models.py
+++ liveview/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class CarData(models.Model):
+    """
+    Class defining the structure of the database that contains the raw data
+    """
+
+    # Fields to define columns and data types
+    tstamp = models.DateTimeField() # Time stamp of datetime.datetime class
+    temperature = models.FloatField() # Hexadecimal 
+    soc = models.FloatField()
+    binary_var = models.BinaryField()
+    # Etc ... 
+
+
+class CarDataTypes(models.Model):
+    """
+    Class defining the datatypes to convert the raw data into readable formats in the charts
+    """
+
+    # Fields
