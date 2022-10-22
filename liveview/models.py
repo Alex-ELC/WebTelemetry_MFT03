eabdfd85b0cf43202b8adc34bc3cee666ff10665
alex13217 <alexgilbert215@gmail.com>
2022-10-22 18:36:18 +0200

    Modified models

--- liveview/models.py
+++ liveview/models.py
@@ -1,26 +1,14 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 
-class CarData(models.Model):
+class DataPoint(models.Model):
     """
     Class defining the structure of the database that contains the raw data
     """
 
     # Fields to define columns and data types
-    tstamp = models.DateTimeField() # Time stamp of datetime.datetime class
-    temperature = models.FloatField() # Hexadecimal 
-    soc = models.FloatField()
-    a_x = models.FloatField()
-    a_y = models.FloatField()
-    a_z = models.FloatField()
-    binary_var = models.BinaryField()
-    # Etc ... 
+    timestamp = models.DateTimeField() # Time stamp of datetime.datetime class
+    value = models.IntegerField() # Generic value type
+    data_type = models.CharField(max_length=20) # Indicating the data type
 
-
-class CarDataTypes(models.Model):
-    """
-    Class defining the datatypes to convert the raw data into readable formats in the charts
-    """
-
-    # Fields
-    pass
